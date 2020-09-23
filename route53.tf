4055993f250902206e80954ec8c7cb62748e9b97
MH4GF <rixi436@gmail.com>
2020-09-23 11:08:27 +0900

    add A record

--- route53.tf
+++ route53.tf
@@ -1,3 +1,13 @@
 resource "aws_route53_zone" "cosmic-night-run" {
   name = "cosmicnight.run"
 }
+
+/* ブログ用 https://cosmicnight.run */
+resource "aws_route53_record" "cosmic-night-run" {
+  name    = aws_route53_zone.cosmic-night-run.name
+  zone_id = aws_route53_zone.cosmic-night-run.id
+  type    = "A"
+  records = ["76.76.21.21"]
+  ttl     = 300
+}
+
