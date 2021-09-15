b8886e6a27792ff03a3bab9c471c4da6c6d6e17f
MH4GF <rixi436@gmail.com>
2021-09-15 14:43:38 +0900

    GitHub Actionsでterraformへのアクセスはしない
    ReadOnlyAccessだとしてもpublicリポジトリのGitHub Actionsに権限を渡しておくのは危険すぎる。
    やるとしたらAssumeRoleで解決する

--- iam.tf
+++ iam.tf
@@ -1,22 +1,3 @@
 resource "aws_iam_account_alias" "ort-dev" {
   account_alias = "ort-dev"
 }
-
-# https://github.com/MH4GF/terraform-aws のGitHub Actions
-resource "aws_iam_user" "github-actions" {
-  name = "github-actions"
-}
-
-# GPG鍵の管理が辛いので、コンソール上で作ったアクセスキーをimportした
-resource "aws_iam_access_key" "github-actions" {
-  user = aws_iam_user.github-actions.name
-}
-
-data "aws_iam_policy" "read-only-access" {
-  arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
-}
-
-resource "aws_iam_user_policy_attachment" "github-actions" {
-  user       = aws_iam_user.github-actions.name
-  policy_arn = data.aws_iam_policy.read-only-access.arn
-}
